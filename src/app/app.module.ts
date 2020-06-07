dba53bd84ba1eafb16423a53a095f4701fb1c22c
denis <dineresc@gmail.com>
2020-06-07 19:55:08 -0300

    importing angular material modules for new post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { 
+  MatInputModule, 
+  MatCardModule,
+  MatToolbarModule,
+  MatButtonModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NewPostComponent } from './posts/new-post/ new-post.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewPostComponent
+    NewPostComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/new-post/ new-post.component.ts
+++ src/app/posts/new-post/ new-post.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-new-post',
-    templateUrl: './new-post.component.html'
+    templateUrl: './new-post.component.html',
+    styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent {
 
